672968b29b50718ef749ffc23fd62f1d336d7fbd
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2020-11-06 15:18:39 +0300

    Update examples

--- examples/createToken.php
+++ examples/createToken.php
@@ -7,8 +7,8 @@ require_once("./../vendor/autoload.php");
 
 $directPayOnline = new DirectPayOnline("9F416C11-127B-4DE2-AC7F-D5710E4C5E0A");
 $transaction = new Transaction(100, "USD");
-$service1 = new Service("Shirt", 3854, "2020/02/12 11:21");
-$service2 = new Service("Trouser", 3854, "2020/02/12 11:21");
+$service1 = new Service("Test Product", 3854, "2020/02/12 11:21");
+$service2 = new Service("Test Service", 5525, "2020/02/12 11:21");
 
 $createToken = $directPayOnline->createToken(
 	$transaction,
