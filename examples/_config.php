52d39a8b9c50a8f5700583f4b11792df04513b97
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2020-11-10 11:08:44 +0300

    Test mode enabled

--- examples/_config.php
+++ examples/_config.php
@@ -6,6 +6,6 @@ require_once("./../vendor/autoload.php");
 // Config
 $config = new Config();
 $config->setCompanyToken("9F416C11-127B-4DE2-AC7F-D5710E4C5E0A");
-$config->setTestMode(false);
+$config->setTestMode(true);
 
 return $config;
